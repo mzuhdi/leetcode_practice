ca00a7f80435f927e1b8e98e4ea29a24ea84ee0e
Muhammad Zuhdi <Muhammad.Zuhdi07@gmail.com>
2021-05-20 11:20:42 -0400

    intial contains duplicate solution

---
+++ Contains Duplicate/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    /* Given an integer array nums,
+    return true if any value appears at least twice in the array,
+    and return false if every element is distinct.*/
+
+    public static void main(String[] args){
+
+    }
+
+    public static boolean containsDuplicate(int[] nums){
+
+
+        return true;
+    }
+}
