24b6b676f89e74d49db997e289eaebe69d9fbc19
Muhammad Zuhdi <Muhammad.Zuhdi07@gmail.com>
2021-05-20 11:39:33 -0400

    hashset solution

--- Contains Duplicate/src/Main.java
+++ Contains Duplicate/src/Main.java
@@ -1,16 +1,42 @@
+import java.util.HashSet;
+
 public class Main {
 
     /* Given an integer array nums,
     return true if any value appears at least twice in the array,
-    and return false if every element is distinct.*/
+    and return false if every element is distinct.
+    Input: nums = [1,2,3,1]
+    Output: true
+    */
 
     public static void main(String[] args){
+        int[] test = new int[] {1, 2, 3, 1};
+        int[] test2 = new int[] {1, 2, 3, 4};
+
+        boolean result1 = containsDuplicateHash(test);
+        System.out.println(result1);
+
+        boolean result2 = containsDuplicateHash(test2);
+        System.out.println(result2);
 
     }
 
-    public static boolean containsDuplicate(int[] nums){
+    public static boolean containsDuplicateHash(int[] nums){
 
+        //initalize hashset of int
+        //iterate through the array
+            // if the number is not the set
+                // add it
+                //  otherwise return true
 
-        return true;
+        HashSet<Integer> hs = new HashSet<Integer>();
+        for (int i : nums){
+            if (!hs.contains(i)){
+                hs.add(i);
+            } else {
+                return true;
+            }
+        }
+        return false;
     }
 }
